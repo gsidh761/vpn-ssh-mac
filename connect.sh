8696b197d409ce4f0d8ec8b88167439b131d28b3
Gureet Sidhu <gsidh761@mtroyal.ca>
2025-09-15 11:21:50 -0600

    Adds connect.sh file...

---
+++ connect.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Auto Connect
+# Type connect In Terminal To Start The Tunnellblick VPN Service & SSH 
+
+# Currently It Only Works On MacOS (Due To Osascript)
+# Doesn't Require Any Third Party Packages (Aside From The Tunnelblick Application)
+
+# Gsidh761
+
+clear 
+echo "Starting VPN Service..."
+
+# Start VPN Service
+osascript -e 'tell application "Tunnelblick" to connect "macovpn-config"' 2>/dev/null
+if [ $? -eq 0 ]; then
+    echo "VPN Has Been Connected. Proceeding..."
+else
+    echo "VPN Has Not Been Connected. Quitting..."
+    exit 1
+fi
+
+# Free Any Open Ports (:22)
+free_port(){
+    echo "Killing Any Pid's On Port 22 (SSH)"
+    sudo lsof -ti :22 | xargs sudo kill -9 2>/dev/null
+}
+
+# Quit Tunnelblick
+quit_vpn_app(){
+    killall -9 "Tunnelblick" 2>/dev/null
+}
+
+# Cleanup After The Program Has Halted
+cleanup() {
+    echo
+    echo "Stopping VPN Service..."
+    osascript -e 'tell application "Tunnelblick" to disconnect all' 2>/dev/null
+
+    # TODO: This doesn't work well. 
+    if [ $? -eq 0 ]; then
+        echo "VPN Has Been Disconnected. Quitting..."
+        free_port
+        quit_vpn_app
+        exit 0
+    else
+        echo "VPN Is Still Connected. Trying Again..."
+	quit_vpn_app
+        cleanup
+    fi
+}
+
+# Makes Sure The Cleanup Function Is Run After Pressing CTRL + Key & Any Other Exit Methods
+trap cleanup SIGINT SIGTSTP EXIT
+
+sleep 2
+clear
+
+free_port
+
+# Starts SSH 
+
+clear
+echo "SSH Starting..."
+
+ssh 
+
+
+
